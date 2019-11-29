22a9b0f1e52611472f0083008a438a9259e61478
patricia-salles <patriciase.eq@gmail.com>
2019-11-28 23:42:59 -0300

    neural network

--- Artificial_Neural_Networks/neural_network.py
+++ Artificial_Neural_Networks/neural_network.py
@@ -14,10 +14,11 @@ cd C:\Users\pati_\Documents\Motor\motores\Artificial_Neural_Networks
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
+##exclui as features de ID
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
-# Encoding categorical data
+# Encoding categorical  - transformando os valores categóricos, p.ex Feminino/Masculino 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
@@ -31,18 +32,47 @@ X = X[:, 1:]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling - padronizar os dados
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Neural Network!
+# Neural Network!
 
 # Importing the Keras libraries and packages
 import keras
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential  ### iniciar a rede neural
+from keras.layers import Dense   ### adicionar as camadas ocultas
 
 # Initialising the neural network
 classifier = Sequential()
+
+'''
+Step1 Inicializar os pesos randomicamente para valores próximo de zero
+Step2 colocar a primeira observação do dataset na camada de input, cada variável 
+em um nó de input - nós temos 11 variáveis, assim teremos 11 input nodes
+Step3 Forward propagation - da esquerda pra direita
+Step4 Compara o valor predito com o valor atual e gera o erro
+Step5 Backpropagation - da direita pra esquerda
+Step 6 Repetir 1 ao 5 e atualiza os pesos depois de cada observação (Reinforcement Learning)
+Repetit 1 ao 5 mas só atualiza os pesos depois "batch observations" (Batch Learning)
+
+'''
+## adicionar a camada de input e o primeira camada oculta
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation = 'relu', input_dim = 11))   
+
+ ## output_dim - quantos nós tem nas camadas ocultas - "dicas/experimentação"
+##pode fazer uma média entre quantidade de variáveis e qtd de resposta no output
+## init = uniform garante que os pesos iniciarão em números próximos de zero
+##input_dim - número de variáveis independentes, número de nós no camada de input
+
+## adicionar a segunda camada oculta
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation = 'relu'))   
+
+## adicionar camada de output
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation = 'sigmoid'))   
+## units=1 one vs all método - se a resposta tiver mais de duas categorias de resposta, p.ex 4 units=4
+## activation = sigmoid, mas se tiver mais de duas categorias de resposta, aí é soft max function
+# Fazendo as predições da base de teste
+y_pred=classifier.predict(X_test)
